84efb1bdca47cde78d0f957a0ab4dea8e02e8de3
Sumit <sumitkumar703011@gmail.com>
2024-05-10 23:52:38 +0530

    Added React Router to handle Single Page Application

--- src/App.js
+++ src/App.js
@@ -3,40 +3,51 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextArea from './components/TextArea';
 import Alert from './components/Alert';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import About from './components/About'
 function App() {
-  const [theme,setTheme] = useState('light');
-  const [alert,setAlert] = useState(null);
+  const [theme, setTheme] = useState('light');
+  const [alert, setAlert] = useState(null);
 
   // Handle DarkMode theme this function is passed to navbar component
-  const toggleTheme = ()=>{
-    if (theme ==='light') {
+  const toggleTheme = () => {
+    if (theme === 'light') {
       setTheme('dark');
       document.body.style.backgroundColor = '#091527';
-      showAlert("Dark Mode Enabled",'success');
+      showAlert("Dark Mode Enabled", 'success');
     }
-    else{
+    else {
       setTheme('light');
       document.body.style.backgroundColor = 'white';
-      showAlert("Dark Mode Disabled",'light');
-    } 
+      showAlert("Dark Mode Disabled", 'light');
+    }
   }
   // Function to handle alert pass to any component
-  const showAlert = (msg,type)=>{
+  const showAlert = (msg, type) => {
     setAlert({
-      msg:msg,
-      type:type
+      msg: msg,
+      type: type
     });
-    setTimeout(()=>{
+    setTimeout(() => {
       setAlert(null);
-    },1500);
+    }, 1500);
   }
 
   return (
     <>
-    <Navbar title = "Text Convertor" aboutText = "About Us" mode={theme} toggleTheme={toggleTheme}/>
-    <Alert alert={alert}/>
-    <TextArea heading="Enter your text here" mode={theme} showAlert={showAlert}/>
-     
+      <BrowserRouter>
+        <Navbar title="Text Convertor" aboutText="About Us" mode={theme} toggleTheme={toggleTheme} />
+        <Alert alert={alert} />
+        <Routes>
+          <Route  path='/' element={<TextArea heading="Enter your text here" mode={theme} showAlert={showAlert} />} />
+          <Route  path='/about' element={<About />} />
+        </Routes>
+
+      </BrowserRouter>
+
+
+
+
     </>
   );
 }
