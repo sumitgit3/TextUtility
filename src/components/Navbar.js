4d10e9f36acef12fe42b770d303b890fa56ddc2c
sumitgit3 <sumitkumar703011@gmail.com>
2024-05-11 23:58:25 +0530

    Fixed Responsive design of Dark mode button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 export default function Navbar(props) {
-  
+
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
       <div className="container-fluid">
@@ -19,14 +19,15 @@ export default function Navbar(props) {
               <Link className="nav-link" to="/about">{props.aboutText}</Link>
             </li>
             <li className='nav-item'>
-              
+
             </li>
           </ul>
+          <div className={`form-check form-switch text-${props.mode === 'light' ? 'dark' : 'light'} `}>
+            <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={props.toggleTheme} />
+            <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+          </div>
         </div>
-        <div className={`form-check form-switch text-${props.mode ==='light'?'dark':'light'} `}>
-          <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onChange={props.toggleTheme}/>
-          <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
-        </div>
+
       </div>
     </nav>
   );
